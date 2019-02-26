fae7b4ce67d1931aa48028ed40f981973ea6529a
Gema Rico Pozas <UO238096@uniovi.es>
2019-02-26 16:08:00 +0100

    Creado esquema general de la aplicación

---
+++ sdi-entrega1-310/src/main/java/com/mywallapop/SdiEntrega1310Application.java
@@ -0,0 +1,13 @@
+package com.mywallapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega1310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega1310Application.class, args);
+	}
+
+}
---
+++ sdi-entrega1-310/src/test/java/com/mywallapop/SdiEntrega1310ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mywallapop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdiEntrega1310ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
